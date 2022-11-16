cc3f07369bb212c286af97440db5234f01100504
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-11-16 10:49:03 -0800

    another update to shell project

--- shell.h
+++ shell.h
@@ -13,27 +13,24 @@
 #include <errno.h>
 
 /* for read/write buffers */
-
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
 /* for command chaining */
-
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
 /* for convert_number() */
-
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
 /* 1 if using system getline() */
-
 #define USE_GETLINE 0
 #define USE_STRTOK 0
+
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
 
@@ -92,6 +89,7 @@ typedef struct passinfo
 	char **environ;
 	int env_changed;
 	int status;
+
 	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
@@ -125,7 +123,6 @@ char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
 
 /* loophsh.c (FILE DELETED AND FUNCTION NOT USED)*/
-
 /* int loophsh(char **); */
 
 /* toem_errors.c */
